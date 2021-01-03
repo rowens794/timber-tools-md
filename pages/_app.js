8d1db3ebbd5669b3f5b83c5e7f925f9864f313d2
rowens794 <rowens794@gmail.com>
2021-01-02 22:22:13 -0500

    added ga tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/index.css'
+import React from "react";
+import ReactGA from "react-ga";
+
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  let production = true;
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") production = false;
+
+  if (typeof window !== "undefined" && production) {
+    ReactGA.initialize("UA-96829474-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  return <Component {...pageProps} />;
 }
