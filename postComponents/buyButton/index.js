9a85029b20c3786ec59c6aed9be263e356bd61e7
rowens794 <rowens794@gmail.com>
2021-01-06 15:01:14 -0500

    added ga tracking

--- postComponents/buyButton/index.js
+++ postComponents/buyButton/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import ReactGA from "react-ga";
 
 import styles from "./buyButton.module.css";
 
 export default function index({ productLink = null, text = "See Price & Reviews on Amazon" }) {
+  let clickHandler = () => {
+    ReactGA.event({
+      category: "outbound",
+      action: "click",
+      Label: productLink,
+      value: 0.1,
+    });
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={clickHandler}>
       <a className={styles.link} href={productLink}>
         <p className={styles.button1}>{text}</p>
       </a>
