346c8437aa9c00ff7c12a6619cc36390092f9eed
rowens794 <rowens794@gmail.com>
2021-01-06 20:41:45 -0500

    removed folder for bwrr

--- best-wood-router-reviews.js/postBody.js
+++ best-wood-router-reviews.js/postBody.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 import Head from "next/head";
 
 import styles from "../../postComponents/postStyles.module.css";
-import HeroImage from "../../postComponents/heroImage";
-import PostTitle from "../../postComponents/postTitle";
-import ProductImage from "../../postComponents/productImg";
-import BuyButton from "../../postComponents/buyButton";
-import InContentProductLink from "../../postComponents/inContentProductLink";
+import HeroImage from "../postComponents/heroImage";
+import PostTitle from "../postComponents/postTitle";
+import ProductImage from "../postComponents/productImg";
+import BuyButton from "../postComponents/buyButton";
+import InContentProductLink from "../postComponents/inContentProductLink";
 
 export default function index({ props = { title: null, coverImage: null, excerpt: null } }) {
   let { title, coverImage, excerpt } = props;
---
+++ pages/best-wood-router-reviews.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+import Header from "../components/header";
+import Footer from "../components/footer";
+// import PostBody from "./postBody";
+import ReadMore from "../components/read-more";
+import stories from "../postComponents/postList";
+import styles from "../postComponents/postStyles.module.css";
+import helpers from "../postComponents/helpers";
+
+export default function index({ postObj }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className={styles.postWrapper}>
+        <Header />
+        {/* <PostBody props={postObj[router.pathname.replace("/", "")]} /> */}
+        <ReadMore posts={stories} />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  let postObj = helpers.getTitleAndHero();
+
+  return {
+    props: { postObj },
+  };
+}
