36470494ffcfeb35372bcb1bbe2414b88fe1fb28
rowens794 <rowens794@gmail.com>
2021-01-06 20:45:26 -0500

    renamed bwrr folder stupid error

--- pages/best-wood-router-reviews/index.js
+++ pages/best-wood-router-reviews/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-import Header from "../components/header";
-import Footer from "../components/footer";
-// import PostBody from "./postBody";
-import ReadMore from "../components/read-more";
-import stories from "../postComponents/postList";
-import styles from "../postComponents/postStyles.module.css";
-import helpers from "../postComponents/helpers";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import PostBody from "./postBody";
+import ReadMore from "../../components/read-more";
+import stories from "../../postComponents/postList";
+import styles from "../../postComponents/postStyles.module.css";
+import helpers from "../../postComponents/helpers";
 
 export default function index({ postObj }) {
   const router = useRouter();
@@ -16,7 +16,7 @@ export default function index({ postObj }) {
     <>
       <div className={styles.postWrapper}>
         <Header />
-        {/* <PostBody props={postObj[router.pathname.replace("/", "")]} /> */}
+        <PostBody props={postObj[router.pathname.replace("/", "")]} />
         <ReadMore posts={stories} />
       </div>
       <Footer />
