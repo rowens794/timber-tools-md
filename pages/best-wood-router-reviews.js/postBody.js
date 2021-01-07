56268f04a02eb4d1a2b14dcdadfbf99cceaeecd0
rowens794 <rowens794@gmail.com>
2021-01-06 20:24:37 -0500

    1

--- pages/best-wood-router-reviews.js/postBody.js
+++ pages/best-wood-router-reviews.js/postBody.js
@@ -5,9 +5,9 @@ import Head from "next/head";
 import styles from "../../postComponents/postStyles.module.css";
 import HeroImage from "../../postComponents/heroImage";
 import PostTitle from "../../postComponents/postTitle";
-import ProductImage from "../../postComponents/productImg";
-import BuyButton from "../../postComponents/buyButton";
-import InContentProductLink from "../../postComponents/inContentProductLink";
+// import ProductImage from "../../postComponents/productImg";
+// import BuyButton from "../../postComponents/buyButton";
+// import InContentProductLink from "../../postComponents/inContentProductLink";
 
 export default function index({ props = { title: null, coverImage: null, excerpt: null } }) {
   let { title, coverImage, excerpt } = props;
