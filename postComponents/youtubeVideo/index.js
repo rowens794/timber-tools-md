3f27fd226b830d826b200ebb6caf5efedd24d8aa
rowens794 <rowens794@gmail.com>
2023-01-15 11:17:08 -0500

    updated video dependency

--- postComponents/youtubeVideo/index.js
+++ postComponents/youtubeVideo/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Youtube from "react-lazyload-youtube";
-import "react-lazyload-youtube/dist/index.css";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 import styles from "./youtubeVideo.module.css";
 
@@ -8,7 +8,12 @@ export default function index({ videoId }) {
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
-        <Youtube width="400px" height="250px" videoId={videoId} className={styles.container} />
+        <div className="w-96 aspect-video">
+          <LiteYouTubeEmbed
+            id={videoId} // Default none, id of the video or playlist
+            noCookie={true} //Default false, connect to YouTube via the Privacy-Enhanced Mode using https://www.youtube-nocookie.com
+          />
+        </div>
       </div>
     </div>
   );
